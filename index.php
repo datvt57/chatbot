aac5714aa962ee21643543be36ef0fe76ada7478
datvt57 <51524885+datvt57@users.noreply.github.com>
2020-03-18 12:54:12 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
 include 'model/class.php';
 header("Access-Control-Allow-Origin: *");
 $reply = new Message();
-$reply->message[] = new Text("Welcome to chatfuel");
-$reply->message[] = new Text("What are you up to?");
+$reply->messages[] = new Text("Welcome to chatfuel");
+$reply->messages[] = new Text("What are you up to?");
 echo json_encode($reply);
 http_response_code(200);
  ?>
